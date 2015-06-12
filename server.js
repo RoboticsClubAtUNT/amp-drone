af06db622050b7d1bb6fa9cb28a0c0381a3caa40
jeffanderson <jeff.anderson785@gmail.com>
2015-06-11 21:11:25 -0500

    changed the port

--- server.js
+++ server.js
@@ -33,12 +33,12 @@ app.get('/', function (req, res)
 
 
 
-var server = app.listen(3000, function () {
+var server = app.listen(92, function () {
 
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Amp-Drone app listening at http://%s:%s', host, port);
 
 });
 
