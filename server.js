4f562fc60a381434016dbc442088db9e2a896343
AcidRobot <AcidRobot@users.noreply.github.com>
2015-06-28 19:15:29 -0500

    picture

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var camera = new RaspiCam({
     w: 640,
     h: 480,
     output: '/home/pi/node_programs/amp-drone/public/robot.jpg',
-    timeout: 15000,
+    timeout: 1000,
     encoding: 'jpg'
     });
 
